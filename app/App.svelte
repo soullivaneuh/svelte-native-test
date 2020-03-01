695d7e4eddf18b6d8845ba6cf82d7ad4051eb5a5
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-03-01 19:59:02 +0100

    Native IO + FeathersJS

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,7 @@
-<page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script>
-    let message = "Blank Svelte Native App"
+  import { SocketIO } from 'nativescript-socketio';
+  import feathers from '@feathersjs/client';
+  // The following import causes the crash:
+  import socketio from '@feathersjs/socketio-client';
+  import auth from '@feathersjs/authentication-client';
 </script>
-
-<style>
-    .info .fas {
-        color: #3A53FF;
-    }
-    .info {
-        font-size: 20;
-    }
-</style>
